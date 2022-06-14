c522c4ddd919e549893bb6ebd45e0747b635e272
fakhrimuh <muhammad.fakhri1234@gmail.com>
2022-06-14 20:33:14 +0700

    add button to prototype

--- resources/views/frontend/layout/navbar.blade.php
+++ resources/views/frontend/layout/navbar.blade.php
@@ -1,77 +1,41 @@
 <nav class="navbar navbar-expand-lg navbar-light fixed-top py-5 d-block" data-navbar-on-scroll="data-navbar-on-scroll">
-    <div class="container">
-      <a class="navbar-brand" href="/">
-        <img src="/images/logotranspr.png" height="40" alt="logo" />
-      </a>
-      <button
-        class="navbar-toggler"
-        type="button"
-        data-bs-toggle="collapse"
-        data-bs-target="#navbarSupportedContent"
-        aria-controls="navbarSupportedContent"
-        aria-expanded="false"
-        aria-label="Toggle navigation"
-      >
-        <span class="navbar-toggler-icon"> </span>
-      </button>
-      <div
-        class="collapse navbar-collapse border-top border-lg-0 mt-4 mt-lg-0"
-        id="navbarSupportedContent"
-      >
-        <ul class="navbar-nav ms-auto pt-2 pt-lg-0 font-base align-items-lg-center align-items-start">
-          <li class="nav-item px-0 px-xl-3">
-            <a
-              class="nav-link text-kuning fw-medium"
-              aria-current="page"
-              href="/about-us"
-              ><i class="fas fa-question-circle text-biru"></i> About
-              Us</a
-            >
-          </li>
-          <li class="nav-item px-0 px-xl-3">
-            <a
-              class="nav-link text-kuning fw-medium"
-              aria-current="page"
-              href="/#services"
-              >Services</a
-            >
-          </li>
-          <li class="nav-item px-0 px-xl-3">
-            <a class="nav-link text-kuning fw-medium" aria-current="page" href="/#destination">Destination</a>
-          </li>
+  <div class="container">
+    <a class="navbar-brand" href="/">
+      <img src="/images/logotranspr.png" height="40" alt="logo" />
+    </a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"> </span>
+    </button>
+    <div class="collapse navbar-collapse border-top border-lg-0 mt-4 mt-lg-0" id="navbarSupportedContent">
+      <ul class="navbar-nav ms-auto pt-2 pt-lg-0 font-base align-items-lg-center align-items-start">
+        <li class="nav-item px-0 px-xl-3">
+          <a class="nav-link text-kuning fw-medium" aria-current="page" href="/about-us"><i class="fas fa-question-circle text-biru"></i> About
+            Us</a>
+        </li>
+        <li class="nav-item px-0 px-xl-3">
+          <a class="nav-link text-kuning fw-medium" aria-current="page" href="/#services">Services</a>
+        </li>
+        <li class="nav-item px-0 px-xl-3">
+          <a class="nav-link text-kuning fw-medium" aria-current="page" href="/#destination">Destination</a>
+        </li>
 
-          <li class="nav-item px-0 px-xl-3">
-              <a class="nav-link text-kuning fw-medium"
-                 aria-current="page"
-                 href="/#prototype">Prototype
-              </a>
-          </li>
+        <li class="nav-item px-0 px-xl-3">
+          <a class="nav-link text-kuning fw-medium" aria-current="page" href="/#prototype">Prototype
+          </a>
+        </li>
 
-          <li class="nav-item px-0 px-xl-3">
-            <a
-              class="nav-link text-kuning fw-medium"
-              aria-current="page"
-              href="/#testimonial"
-              >Testimonial</a
-            >
-          </li>
-          <li class="nav-item px-0 px-xl-3">
-            <a
-              class="nav-link text-kuning fw-medium"
-              aria-current="page"
-              href="/#partners"
-              >Partners
-            </a>
-          </li>
-          {{--<li class="nav-item px-0 px-xl-3">
-            <a
-              class="nav-link text-kuning fw-medium"
-              aria-current="page"
-              href="login.html">
-                <span class="fas fa-sign-in"></span> Login
-            </a>
-          </li>--}}
-        </ul>
-      </div>
+        <li class="nav-item px-0 px-xl-3">
+          <a class="nav-link text-kuning fw-medium" aria-current="page" href="/#testimonial">Testimonial</a>
+        </li>
+        <li class="nav-item px-0 px-xl-3">
+          <a class="nav-link text-kuning fw-medium" aria-current="page" href="/#partners">Partners
+          </a>
+        </li>
+        <li class="nav-item px-0 px-xl-3">
+          <a class="nav-link text-white btn-primary btn-lg me-md-4 mb-3 mb-md-0 border-0 primary-btn-shadow" href="#aboutus ">Lets Travel
+          </a>
+        </li>
+      </ul>
     </div>
-  </nav>
+  </div>
+</nav>
